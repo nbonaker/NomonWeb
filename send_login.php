b19193b3af9e1689a3c35afbb7cc903550e2fea7
nbonaker <nbonaker@mit.edu>
2020-05-05 17:51:08 -0400

    --php database login updated html file

--- send_login.php
+++ send_login.php
@@ -8,6 +8,7 @@ $result = 0;
 
 /* Create connection */
 $conn = new mysqli($host, $username, $password, $dbname);
+
 /* Check connection */
 if ($conn->connect_error) {
     die("Connection to database failed: " . $conn->connect_error);
@@ -15,38 +16,21 @@ if ($conn->connect_error) {
 
 /* Get data from Client side using $_POST array */
 $uname  = $_POST['user_id'];
-/* validate whether user has entered all values. */
-var numbers = /^[0-9]+$/;
-if(!$uname){
-    $result = 2;
-}else (!inputtxt.value.match(numbers)) {
-    $result = 3;
-}
-else {
-    $id_result_array = array();
-    $uname = parseInt($uname)
-    //check if user ID already exists:
-    $sql_id_query = "SELECT count(*) FROM user_info WHERE id = (?)  ";
-    $stmt_id_query   = $conn->prepare($sql_id_query);
-    $result = $conn->query($stmt_id_query);
+$id_result_array = array();
 
-    if ($result->num_rows > 0) {
+$uname = (int)$num;
+$sql_id_query = "SELECT count(*) FROM user_info WHERE id = (?)  ";
+$stmt_id_query   = $conn->prepare($sql_id_query);
+$result = $conn->query($stmt_id_query);
+
+if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         array_push($id_result_array, $row);
     }
-    }
-    echo $id_result_array;
-
-
-    //SQL query to get results from database
-    //$sql    = "insert into users (user_id) values (?)  ";
-    //$stmt   = $conn->prepare($sql);
-    //$stmt->bind_param('sss', $uname);
-    //if($stmt->execute()){
-    //    $result = 1;
-    //}
 }
-echo $result;
+
+echo json_encode($result_array);
+
 $conn->close();
 
 ?>
