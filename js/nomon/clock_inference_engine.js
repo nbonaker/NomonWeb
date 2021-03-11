bbf8d751de7ffbbc8feb0cba7c1b5383ffaf0236
nbonaker <nbonaker@mit.edu>
2021-03-11 12:58:09 -0500

    --added click_dist monitoring to tutorial --initial click dist monitoring in regular nomon

--- js/nomon/clock_inference_engine.js
+++ js/nomon/clock_inference_engine.js
@@ -217,6 +217,7 @@ export class ClockInference{
         this.kde.increment_dens(yin, this.kde.ksigma);
         this.kde.calc_ksigma(this.n_hist, Math.min(this.kde.n_ksigma, this.kde.y_li.length));
         this.parent.histogram.update(this.kde.dens_li);
+        this.parent.moniter_click_dist();
     }
     update_scores(time_diff_in) {
         var clock_locs = [];
@@ -246,6 +247,7 @@ export class ClockInference{
             this.kde.increment_dens(this.kde.y_li[n], this.kde.y_ksigma[n]);
         }
         this.kde.calc_ksigma(this.n_hist, Math.min(this.kde.n_ksigma, this.kde.y_li.length));
+        this.parent.moniter_click_dist();
     }
     update_history(time_diff_in){
         var clock_history_temp = [];
